23b48425e291c80a230670610801e9b2161e9e88
josephfernicola <josephrfernicola@gmail.com>
2022-07-30 20:56:24 -0700

    display correct user info for post

--- src/components/ProfilePostContainer.js
+++ src/components/ProfilePostContainer.js
@@ -12,21 +12,38 @@ import {
   arrayUnion,
 } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { useNavigate, Link, Navigate } from "react-router-dom";
+import { useNavigate, Link, Navigate, useLocation } from "react-router-dom";
 
 const ProfilePostContainer = (props) => {
   const { userPosts, setUserPosts } = props;
+  const location = useLocation();
 
   const navigate = useNavigate();
 
-  const switchToPostPage = (e) => {
-    userPosts.forEach((image) => {
-      if (image.image === e.target.src) {
-        navigate(`/post/${getAuth().currentUser.uid}/${image.postId}`, {
-          state: { image: e.target.src },
+  const switchToPostPage = async (e) => {
+    const users = await getFirestore();
+    const usersRef = await collection(users, "users");
+    getDocs(usersRef)
+      .then((snapshot) => {
+        let users = [];
+        snapshot.docs.forEach((doc) => {
+          users.push({ ...doc.data(), id: doc.id });
         });
-      }
-    });
+        users.forEach((user) => {
+          if (location.pathname.includes(user.uid)) {
+            userPosts.forEach((image) => {
+              if (image.image === e.target.src) {
+                navigate(`/post/${user.uid}/${image.postId}`, {
+                  state: { image: e.target.src },
+                });
+              }
+            });
+          }
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
   return (
     <div className="profilePostContainer">
